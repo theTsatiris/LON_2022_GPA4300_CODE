d2ccb74b4a196608594300484abbf6226aabdcaa
GTsatiris <george.tsatiris@gmail.com>
2022-03-28 10:47:14 +0300

    Data structures

---
+++ ObjectOrientedExample/DataStructures.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class DataStructures
+{ 
+    static void Main(string[] args)
+    {
+        Player[] players = new Player[100];
+
+        List<Player> plList = new List<Player>();
+        //plList[0]
+        foreach(Player pl in plList)
+        {
+
+        }
+
+        Dictionary<string, Player> playerDict = new Dictionary<string, Player>();
+
+        playerDict.Add("George123456", new Player("George"));
+        if(playerDict.ContainsKey("George123456"))
+        {
+            playerDict["George123456"] = new Player("Chiara");
+        }
+        else
+        {
+            playerDict.Add("George123456", new Player("George"));
+        }
+
+        foreach(string key in playerDict.Keys)
+        {
+            //playerDict[key]...
+        }
+        foreach(Player pl in playerDict.Values)
+        {
+            //pl
+        }
+
+        foreach(KeyValuePair<string, Player> pair in playerDict)
+        {
+            //pair.Key...
+            //pair.Value...
+        }
+
+    }
+}
--- ObjectOrientedExample/Program.cs
+++ ObjectOrientedExample/Program.cs
@@ -57,62 +57,107 @@ class Player
         return this.ID;
     }
 
-    public int DealDamage(Player opponent)
+    public virtual int DealDamage(Player opponent)
     {
+        Console.WriteLine("base DealDamage...");
         int damage = this.baseAttack - opponent.baseArmor;
         return damage;
-    }
-
-    public int DealDamage(Warrior opponent)
-    {
-        return 0;
     }
 }
 
 class Mage : Player
 {
-    
+    public int baseMagic;
+
+    public Mage()
+    {
+        baseMagic = 100;
+    }
+
+    public Mage(string plName) : base(plName)
+    {
+        baseMagic = 100;
+    }
+
+    public override int DealDamage(Player opponent)
+    {
+        Console.WriteLine("Mage DealDamage...");
+        int initialDamage = base.DealDamage(opponent);
+
+        return initialDamage + baseMagic;
+    }
 }
 
 class Warrior : Player
 {
-    void DoSomething()
+    public int baseRage;
+    public Warrior()
     {
-        this.SetID(1234);
+        baseRage = 1000;
+    }
+
+    public Warrior(string plName) : base(plName)
+    {
+        baseRage = 1000;
     }
 }
 
 class Program
 {
-    static void Main(string[] args)
+    //static void Main(string[] args)
+    //{
+    //    Player Chiara = new Player("Chiara");
+    //    /*int X = 123;
+    //    Chiara.SetID(X);
+    //    Console.WriteLine(X);*/
+    //    /*Console.WriteLine(Chiara.GetID());*/
+    //    Chiara.baseHealth = 10000;
+    //    Chiara.baseAttack = 1000;
+
+    //    Player Morgan = new Player();
+    //    Morgan.plName = "Morgan";
+    //    /*Console.WriteLine(Morgan.GetID());*/
+    //    Morgan.baseHealth = 10000;
+    //    Morgan.baseArmor = 100;
+
+
+    //    Mage George = new Mage("George");
+    //    Warrior Harrison = new Warrior("Harrison");
+
+    //    FightSimulation(Chiara, Morgan);
+    //    FightSimulation(Chiara, George);
+    //    FightSimulation(George, Chiara);
+    //    FightSimulation(Morgan, Chiara);
+    //    FightSimulation(Harrison, George);
+
+
+    //    //Warrior George = new Warrior();
+
+    //    //Mage Chris = new Mage();
+
+    //    //Console.WriteLine(Morgan.baseHealth);
+    //    //int damage = Chiara.DealDamage(Morgan);
+    //    //Morgan.baseHealth -= damage;
+    //    //Console.WriteLine(Morgan.baseHealth);
+
+    //    //Morgan.DealDamage(George);
+
+    //    //Player.TOTAL_PLAYERS = 81237465;
+
+    //}
+
+    static void FightSimulation(Player pl1, Player pl2)
     {
-        Player Chiara = new Player("Chiara");
-        /*int X = 123;
-        Chiara.SetID(X);
-        Console.WriteLine(X);*/
-        Console.WriteLine(Chiara.GetID());
-
-
-        Chiara.baseHealth = 10000;
-        Chiara.baseAttack = 1000;
-
-        Player Morgan = new Player();
-        Console.WriteLine(Morgan.GetID());
-        Morgan.baseHealth = 10000;
-        Morgan.baseArmor = 100;
-
-        //Warrior George = new Warrior();
-
-        //Mage Chris = new Mage();
-
-        //Console.WriteLine(Morgan.baseHealth);
-        //int damage = Chiara.DealDamage(Morgan);
-        //Morgan.baseHealth -= damage;
-        //Console.WriteLine(Morgan.baseHealth);
-
-        //Morgan.DealDamage(George);
-
-        //Player.TOTAL_PLAYERS = 81237465;
-
+        Console.WriteLine(pl1.plName + " VS " + pl2.plName);
+        Console.WriteLine("START OF FIGHT!!!!!");
+        Console.WriteLine("Player 1 health: " + pl1.baseHealth);
+        Console.WriteLine("Player 2 health: " + pl2.baseHealth);
+        Console.WriteLine("Fighting in progress...");
+        pl2.baseHealth -= pl1.DealDamage(pl2);
+        pl1.baseHealth -= pl2.DealDamage(pl1);
+        Console.WriteLine("Player 1 health: " + pl1.baseHealth);
+        Console.WriteLine("Player 2 health: " + pl2.baseHealth);
+        Console.WriteLine("Fighting ended!");
+        Console.WriteLine();
     }
 }
